191902da356be3cf41035bc8f9c966635b25809c
remi-gj <rgarotja@gmail.com>
2025-03-25 09:00:15 +0100

    Update ABCDMatrixOpticsPlotExt.jl

--- ext/ABCDMatrixOpticsPlotExt.jl
+++ ext/ABCDMatrixOpticsPlotExt.jl
@@ -2,6 +2,12 @@ module ABCDMatrixOpticsPlotExt
 
 
 using ABCDMatrixOptics, Colors, Interpolations, RecipesBase
+export beamplot3d
+
+# mutable struct WithGaussianBeam3d
+	# system::Vector{<:Elements}
+	# beam::GaussianBeam3d
+# end
 
 mutable struct WithGaussianBeam
     system::Vector{<:Element}
@@ -14,6 +20,47 @@ mutable struct WithGeometricBeam
     beam::GeometricBeam
 end
 
+# did not succed to plot 2 shape with a single @receipe ..
+function Myplot3d(system::Vector{<:Elements}, beam::ABCDMatrixOptics.GaussianBeam3d)
+    ds = ABCDMatrixOptics2.discretize(system, 200)
+    N = length(ds) + 1
+    Tz = typeof(float(beam.zpos))
+    wsags = Vector{Tz}(undef, N)
+	wtans = Vector{Tz}(undef, N)
+    zs = Vector{Tz}(undef, N)
+    wsags[1] = ABCDMatrixOptics.wsag(beam)
+	wtans[1] = ABCDMatrixOptics.wtan(beam)
+    zs[1] = beam.zpos
+    beam = beam
+    for i = 1:length(ds)
+        beam = ds[i] * beam
+        wsags[i+1] = ABCDMatrixOptics.wsag(beam)
+		wtans[i+1] = ABCDMatrixOptics.wtan(beam)
+        zs[i+1] = beam.zpos 
+    end
+    xs, ys_sag, ys_tan = vcat(zs, reverse(zs)), vcat(wsags, (-1.0) .* reverse(wsags)), vcat(wtans, (-1.0) .* reverse(wtans))
+	shape_sag = [(xs[i], ys_sag[i], ) for i in 1:length(xs)]
+	shape_tan = [(xs[i], ys_tan[i], ) for i in 1:length(xs)]
+	index_iterfaces = findall(x -> !(x isa FreeSpace), ds)
+	# We look for all non-FreeSpace elements to display them in the final plot
+	z_interfaces = Vector{Tz}(undef, length(index_iterfaces))
+	for i=1:length(z_interfaces)
+		z_interfaces[i] = zs[index_iterfaces[i]]
+	end
+	plot(shape_sag, seriestype = :shape, 
+		linecolor = color(beam.λ),
+		fillcolor = color(beam.λ),
+		fillalpha = 0.1,
+		label = "Sag." * string(beam.λ),
+		xlabel = "Distance along Beam Axis",
+		yguide = "Extend along Beam Axis")
+	plot!(shape_tan, seriestype = :shape,
+		linecolor = color_tan(beam.λ),
+		fillcolor = color_tan(beam.λ),
+		fillalpha = 0.1,
+		label = "Tan." * string(beam.λ))
+	vline!(z_interfaces, primary = false, color = "black")
+end
 
 # type recipe, e.g. for `plot(WithBeam(system, beam))`
 @recipe f(::Type{WithGaussianBeam}, data::WithGaussianBeam) = begin
@@ -117,7 +164,23 @@ function color(λ)
     color = Colors.HSL(hue_of_λ(λ), 1.0, 0.5)
 end
 
+function color_tan(λ)
+	#slightly darker color for tangential display to be different
+    λmin = 380e-9
+    λmax = 800e-9
+    if λ < λmin
+        return Colors.HSL(260.0, 1.0, 0.15)
+    elseif λ > λmax
+        return Colors.HSL(0, 1.0, 0.15)
+    end
+    color = Colors.HSL(hue_of_λ(λ), 1.0, 0.40)
+end
+
 # user recipe, e.g. for `plot(system, beam)`
+# @recipe f(system::Vector{<:Elements}, beam::ABCDMatrixOptics.GaussianBeam3d) =
+	# WithGaussianBeam3d(system, beam)
+# @recipe f(system::Vector{<:Elements}, beam::ABCDMatrixOptics.GaussianBeam) =
+	# WithGaussianBeam3d(system, beam)
 @recipe f(system::Vector{<:Element}, beam::ABCDMatrixOptics.GaussianBeam) =
     WithGaussianBeam(system, beam)
 
