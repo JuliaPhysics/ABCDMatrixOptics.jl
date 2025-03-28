0b1e1e89cbd7c05faa3cc367fa534c8d492f2a89
remi-gj <rgarotja@gmail.com>
2025-03-28 11:16:19 +0100

    Update ABCDMatrixOpticsPlotExt.jl

--- ext/ABCDMatrixOpticsPlotExt.jl
+++ ext/ABCDMatrixOpticsPlotExt.jl
@@ -26,40 +26,40 @@ function Myplot3d(system::Vector{<:Elements}, beam::ABCDMatrixOptics.GaussianBea
     N = length(ds) + 1
     Tz = typeof(float(beam.zpos))
     wsags = Vector{Tz}(undef, N)
-	wtans = Vector{Tz}(undef, N)
+    wtans = Vector{Tz}(undef, N)
     zs = Vector{Tz}(undef, N)
     wsags[1] = ABCDMatrixOptics.wsag(beam)
-	wtans[1] = ABCDMatrixOptics.wtan(beam)
+    wtans[1] = ABCDMatrixOptics.wtan(beam)
     zs[1] = beam.zpos
     beam = beam
     for i = 1:length(ds)
         beam = ds[i] * beam
         wsags[i+1] = ABCDMatrixOptics.wsag(beam)
-		wtans[i+1] = ABCDMatrixOptics.wtan(beam)
+        wtans[i+1] = ABCDMatrixOptics.wtan(beam)
         zs[i+1] = beam.zpos 
     end
     xs, ys_sag, ys_tan = vcat(zs, reverse(zs)), vcat(wsags, (-1.0) .* reverse(wsags)), vcat(wtans, (-1.0) .* reverse(wtans))
-	shape_sag = [(xs[i], ys_sag[i], ) for i in 1:length(xs)]
-	shape_tan = [(xs[i], ys_tan[i], ) for i in 1:length(xs)]
-	index_iterfaces = findall(x -> !(x isa FreeSpace), ds)
-	# We look for all non-FreeSpace elements to display them in the final plot
-	z_interfaces = Vector{Tz}(undef, length(index_iterfaces))
-	for i=1:length(z_interfaces)
-		z_interfaces[i] = zs[index_iterfaces[i]]
-	end
-	plot(shape_sag, seriestype = :shape, 
-		linecolor = color(beam.λ),
-		fillcolor = color(beam.λ),
-		fillalpha = 0.1,
-		label = "Sag." * string(beam.λ),
-		xlabel = "Distance along Beam Axis",
-		yguide = "Extend along Beam Axis")
-	plot!(shape_tan, seriestype = :shape,
-		linecolor = color_tan(beam.λ),
-		fillcolor = color_tan(beam.λ),
-		fillalpha = 0.1,
-		label = "Tan." * string(beam.λ))
-	vline!(z_interfaces, primary = false, color = "black")
+    shape_sag = [(xs[i], ys_sag[i], ) for i in 1:length(xs)]
+    shape_tan = [(xs[i], ys_tan[i], ) for i in 1:length(xs)]
+    index_iterfaces = findall(x -> !(x isa FreeSpace), ds)
+    # We look for all non-FreeSpace elements to display them in the final plot
+    z_interfaces = Vector{Tz}(undef, length(index_iterfaces))
+    for i=1:length(z_interfaces)
+        z_interfaces[i] = zs[index_iterfaces[i]]
+    end
+    plot(shape_sag, seriestype = :shape, 
+            linecolor = color(beam.λ),
+            fillcolor = color(beam.λ),
+            fillalpha = 0.1,
+            label = "Sag." * string(beam.λ),
+            xlabel = "Distance along Beam Axis",
+            yguide = "Extend along Beam Axis")
+    plot!(shape_tan, seriestype = :shape,
+            linecolor = color_tan(beam.λ),
+            fillcolor = color_tan(beam.λ),
+            fillalpha = 0.1,
+            label = "Tan." * string(beam.λ))
+    vline!(z_interfaces, primary = false, color = "black")
 end
 
 # type recipe, e.g. for `plot(WithBeam(system, beam))`
